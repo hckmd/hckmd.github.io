605800d7f65b7cb10bed79e019ccfb442099c90c
hckmd <daniel.hickmott@gmail.com>
2024-12-14 10:31:26 +1100

    Minor tweaks to passphrase widget
    Includes adding a placeholder image for the first time the widget loads

--- projects/js/passphrase-widget.js
+++ projects/js/passphrase-widget.js
@@ -1,33 +1,46 @@
 // Set the text above the generate button to the initial prompt to match dropdowns
 ['word1', 'word2', 'word3', 'word4'].forEach(selectId => randomiseWordSelection(selectId));
 updatePhraseText();
+
+// Hide the passphrase image until an image has actually loaded
+const outputElement = document.getElementById('output');
+outputElement.style.display = 'none';
+
+// Once an image has loaded, hide the placeholder image - we only need to do this once
+outputElement.onload = function () {
+    if (outputElement.style.display == 'none') {
+        document.getElementById('placeholder').style.display = 'none';
+        outputElement.style.display = 'block';	
+    }
+};
 visualisePhrase();
 
+
 function randomiseWordSelection(selectId) {
-    var select = document.getElementById(selectId);
-    var items = select.getElementsByTagName('option');
-    var index = Math.floor(Math.random() * items.length);
+    const select = document.getElementById(selectId);
+    const items = select.getElementsByTagName('option');
+    const index = Math.floor(Math.random() * items.length);
     select.selectedIndex = index;
 }
 
 function updatePhraseText() {
-    var word1 = document.getElementById('word1').value;
-    var word2 = document.getElementById('word2').value;
-    var word3 = document.getElementById('word3').value;
-    var word4 = document.getElementById('word4').value;
+    const word1 = document.getElementById('word1').value;
+    const word2 = document.getElementById('word2').value;
+    const word3 = document.getElementById('word3').value;
+    const word4 = document.getElementById('word4').value;
 
-    var text = `${word1} ${word2} ${word3} ${word4}`;
+    const text = `${word1} ${word2} ${word3} ${word4}`;
     document.getElementById("phrase").innerText = text;
 }
 
 function visualisePhrase() {
-    var word1 = document.getElementById('word1').value;
-    var word2 = document.getElementById('word2').value;
-    var word3 = document.getElementById('word3').value;
-    var word4 = document.getElementById('word4').value;
+    const word1 = document.getElementById('word1').value;
+    const word2 = document.getElementById('word2').value;
+    const word3 = document.getElementById('word3').value;
+    const word4 = document.getElementById('word4').value;
 
-    var imageDir = "https://hckmd.pythonanywhere.com/static/memory-trick-images";
-    var imageSource = `${imageDir}/${word1}_${word2}_${word3}_${word4}.jpg`;
+    const imageDir = "https://hckmd.pythonanywhere.com/static/memory-trick-images";
+    const imageSource = `${imageDir}/${word1}_${word2}_${word3}_${word4}.jpg`;
     document.getElementById('output').src = imageSource;
 }
 
